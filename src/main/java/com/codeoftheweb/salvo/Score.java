d92c1bf3cdad2d67c4550c2625aab86114e3008f
facu vara <sibeeriano@gmail.com>
2021-07-07 13:13:28 -0300

    clase y repo Score

---
+++ src/main/java/com/codeoftheweb/salvo/Score.java
@@ -0,0 +1,73 @@
+package com.codeoftheweb.salvo;
+import com.codeoftheweb.salvo.Game;
+import com.codeoftheweb.salvo.Player;
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Score {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="player_id")
+    private Player player;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="game_id")
+    private Game game;
+
+    private double score;
+
+    private LocalDateTime finishDate;
+
+    public Score() {
+    }
+
+    public Score(Player player, Game game, double score, LocalDateTime finishDate) {
+        this.player = player;
+        this.game = game;
+        this.score = score;
+        this.finishDate = finishDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public LocalDateTime getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(LocalDateTime finishDate) {
+        this.finishDate = finishDate;
+    }
+}
---
+++ src/main/java/com/codeoftheweb/salvo/ScoreRepository.java
@@ -0,0 +1,14 @@
+package com.codeoftheweb.salvo;
+
+import java.util.Date;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+
+
+public interface ScoreRepository extends JpaRepository<Score, Long> {
+
+    // List<Player> findByCreationDate(Date date);
+}
